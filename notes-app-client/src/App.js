99cb51a313d9838b62aa7328e7f57200139f518f
utopier <utopier3368@gmail.com>
2020-11-06 08:49:43 +0900

    Frontend Setting Up Our React App

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './App.css';
+import Routes from './Routes';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
   );
-};
+}
 
 export default App;
