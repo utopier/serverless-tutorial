0b0d43cfd8cfab8636632bb96c71bc87a24d421a
utopier <utopier3368@gmail.com>
2020-11-06 15:08:00 +0900

    Backend Update 3

--- notes-api/infrastructure/test/DynamoDBStack.test.js
+++ notes-api/infrastructure/test/DynamoDBStack.test.js
@@ -1,4 +1,4 @@
-import { expect, haveResource } from '@aws-cdk/assert';
+//import { expect, haveResource } from '@aws-cdk/assert';
 import * as sst from '@serverless-stack/resources';
 import DynamoDBStack from '../lib/DynamoDBStack';
 
