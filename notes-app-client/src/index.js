388103cf48733eaa49736e71959e61d18b94dfda
utopier <utopier3368@gmail.com>
2020-11-09 17:56:12 +0900

    Frontend amplify configure update

--- notes-app-client/src/index.js
+++ notes-app-client/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 import Amplify, { Auth } from 'aws-amplify';
-
+import awsconfig from './aws-exports';
 import config from './config';
 import { initSentry } from './libs/errorLib';
 
@@ -32,6 +32,7 @@ Amplify.configure({
     ],
   },
 });
+Auth.configure(awsconfig);
 
 ReactDOM.render(
   <Router>
