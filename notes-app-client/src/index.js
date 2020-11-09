5607d19887d1b45616ab777410689e62d0cde173
utopier <utopier3368@gmail.com>
2020-11-09 18:11:23 +0900

    Frontend amplify auth configure

--- notes-app-client/src/index.js
+++ notes-app-client/src/index.js
@@ -31,6 +31,13 @@ Amplify.configure({
     ],
   },
 });
+Auth.configure({
+  mandatorySignIn: true,
+  region: config.cognito.REGION,
+  userPoolId: config.cognito.USER_POOL_ID,
+  identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+});
 
 ReactDOM.render(
   <Router>
